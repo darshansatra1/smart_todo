cb628b1df9aaca158251121dd3e642428a51e5c4
cmd-dev <gitwork0nly@gmail.com>
2020-01-07 18:41:37 +0530

    Initial commit

---
+++ lib/Home.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+        drawer: Drawer(),
+        body:
+
+        appBar: AppBar(
+
+      title: Text('text'),
+
+
+    ));
+  }
+}
